135341828acef29180c62fd2437983ecc13cb43c
Christopher D Uranga <atlanteuranga@christophersair.lan>
2022-04-12 22:05:13 -0400

    flask server and database set up

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/state_happiness")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/happinessDB")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
@@ -16,11 +17,10 @@ def home():
 
 # Route to render plot.html template using data from Mongo
 @app.route("/dashboard/<state>")
-def plot():
-    mongo.db.collection.find_one('<state>')
-    
+def plot_state(state):
+    statedb = mongo.db.state_happiness.find_one({'StateName':state})
 # Return template and data
-    return render_template("plot.html") 
+    return render_template("plot.html", state=state, statedb=statedb) 
 
 
 if __name__ == "__main__":
