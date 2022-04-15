7aeb0c1e38d7d9b7a56705fdab134c21bdfacb2f
RobSalazar <95258172+RobSalazar@users.noreply.github.com>
2022-04-14 22:07:35 -0400

    adding stuff

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,126 +1,63 @@
-
-d3.json("projects/Project-3/static/data/state_data.json").then(function(data) {
-    console.log(data)
-    state_dict = data;
+// //Use the D3 library to read in samples.json and change function name to not overwrite original function name
+function optionChanged(selectedID){
+    console.log('-------------------------from html', {{statedb.DATE}});
+    //Pathing to JSON file data 
+    d3.select("#selDataset").html("");   
+  
+  //    //Grab metadata array data for each item aka "ID" and append each ID to an item
+    {{statedb.DATE}}.forEach(item => {
+       d3.select ("#selDataset").append('option').attr('value', item).text(item);
+       });
+  
+  //    //Pass the selected value through JSON for filtering
+    d3.select("#selDataset").node().value = selectedID;
+  
+  //    //Filter metadata based on selected ID
+    var metaDataID = {{statedb.DATE}}.filter(item=> (item.id == selectedID));
+ 
+    //Advanced Challenge Assignment (Optional)
+ 
+    //Adapt the Gauge Chart from https://plot.ly/javascript/gauge-charts/ to plot the weekly washing frequency of the individual.
+    var gauge = d3.select("#gauge");
+    gauge.html(""); 
+    var washFrequency = metaDataID[0].wfreq;
+ 
+    //Create trace for gauge chart and formatting for chart
+    var guageData = [{
+    domain: { x: [0, 1], y: [0, 1] },
+    value: washFrequency,
+    title: { text: "<b>Belly Button Washing Frequency</b><br> (Scrubs Per Week)" },
+    type: "indicator",
+    mode: "gauge+number",     
+       gauge: {
+       axis: { range: [0,9] },
+       bar: { color: "#FFAC61" },
+       steps: [
+          { range: [0, 1], color: "#6BFFFE" },
+          { range: [1, 2], color: "#5DF0EF" },
+          { range: [2, 3], color: "#50E1E0" },
+          { range: [3, 4], color: "#42D3D2" },
+          { range: [4, 5], color: "#35C4C3" },
+          { range: [5, 6], color: "#28B5B4" },
+          { range: [6, 7], color: "#1AA7A6" },
+          { range: [7, 8], color: "#0D9897" },
+          { range: [8, 9], color: "#008A89" }
+          ],
+       threshold: {
+       value: washFrequency
+       }}}
+    ];
+    var gaugeLayout = {  width: 500, 
+                   height: 500, 
+                   margin: { t: 0, b: 0 }, 
+                   };
+    Plotly.newPlot('gauge', guageData, gaugeLayout); 
+                }
+ 
+  //This needed to be added also so gauge would display on initial load
+  optionChangeBonus(940);
+  
+ //  //Added for consistancy
+  d3.select("#selDataset").on('change',() => {
+  optionChangeBonus(d3.event.target.value);
   });
-
-console.log(state_dict)
-
-function PlotState(state){
-    year = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
-   
-
-    let gdp_trace = {
-    x: year,
-    y: state_dict[state]['GDP'],
-    type: 'line', 
-    name: "GDP ($10,000)"
-    };
-
-    let hs_trace = {
-        x: year,
-        y: state_dict[state]['High School'],
-        type: 'line', 
-        name: "High school graduation rate"
-    };
-
-    let college_trace = {
-        x: year,
-        y: state_dict[state]['College'],
-        type: 'line', 
-        name: "Bachelor's or higher rate"
-    };
-
-    let employment_trace = {
-        x: year,
-        y: state_dict[state]['Employment Rate'],
-        type: 'line',
-        name: "Employment rate"
-    };
-
-    // let happiness_trace = {
-    //     x: year,
-    //     y: state['GDP'],
-    //     type: 'line'
-    // };
-
-    let data = [gdp_trace, hs_trace,college_trace,employment_trace];
-
-    let layout = {
-    title: "Happiness of " + state
-    };
-
-    Plotly.newPlot("plot", data, layout);
-}
-PlotState(`${state}`)
-
-// // Creating the map object
-// var myMap = L.map("map", {
-//     center: [39.8283, -98.5795],
-//     zoom: 4
-//   });
-
-  
-//   // Adding the tile layer
-//   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//   }).addTo(myMap);
-
-
-
-// // Getting our GeoJSON data
-// d3.json("/static/data/us_states.json").then(function(data) {
-    
-//   L.geoJson(data).addTo(myMap);
-// });
-
-// d3.json("/static/data/us_states.json").then(function(data) {
-//     // Creating a GeoJSON layer with the retrieved data
-//     L.geoJson(data, {
-//       // Styling each feature (in this case, a neighborhood)
-//       style: function(feature) {
-//         return {
-//           color: "white",
-//           fillOpacity: 0.5,
-//           weight: 1.5
-//         };
-//       },
-//       // This is called on each feature.
-//       onEachFeature: function(feature, layer) {
-//         // Set the mouse events to change the map styling.
-//         layer.on({
-//           // When a user's mouse cursor touches a map feature, the mouseover event calls this function, which makes that feature's opacity change to 90% so that it stands out.
-//           mouseover: function(event) {
-//             layer = event.target;
-//             layer.openPopup();
-//             layer.setStyle({
-//               fillOpacity: 0.9
-            
-//             });
-//           },
-//           // When the cursor no longer hovers over a map feature (that is, when the mouseout event occurs), the feature's opacity reverts back to 50%.
-//           mouseout: function(event) {
-//             layer = event.target;
-//             layer.closePopup();
-//             layer.setStyle({
-//               fillOpacity: 0.5
-//             });
-//           },
-//           // When a feature (neighborhood) is clicked, it enlarges to fit the screen.
-//           click: function(event) {
-//             state = feature.properties.name;
-//             layer.getBounds();
-//             PlotState(state)
-//           }
-//         });
-//         // Giving each feature a popup with information that's relevant to it
-//         layer.bindPopup("<h2>" + feature.properties.name + "</h2>");
-  
-//       }
-//     }).addTo(myMap);
-//   });
-
-//   d3.json("/static/data/state_data.json").then(function(data) {
-    
-//     state_dict = data;
-//   });
